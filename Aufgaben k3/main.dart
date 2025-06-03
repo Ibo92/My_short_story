d7eb30e9c8183730cb6edf98333851e8b8fdd039
Ibo92 <bibomus1@gmail.com>
2025-06-03 12:07:40 +0200

    Aufgabe 1

---
+++ Aufgaben k3/main.dart
@@ -0,0 +1,22 @@
+void main() {
+  String gdStr = "Guten";
+  String dayStr = "Tag";
+  String worldStr = "Welt";
+  String d = "!";
+  String timeStr = "Zeit";
+  String c = "Schöne";
+  String morning = "Morgen";
+  String s = "Heute";
+  String spa = " ";
+
+  //int twanty = 20;
+  int seven = 7;
+  int twelve = 12;
+  int a = 24;
+
+  print(gdStr + spa + dayStr + spa + worldStr + d);            // Guten Tag Welt!
+  print(c + spa + morning + d);                                // Schöne Morgen!
+  print(s + spa + "ist der" + spa + twelve.toString() + "." +
+        seven.toString() + "." + a.toString());                // Heute ist der 12.7.24
+  print("Zeit: " + timeStr);                                   // Zeit: Zeit
+}
